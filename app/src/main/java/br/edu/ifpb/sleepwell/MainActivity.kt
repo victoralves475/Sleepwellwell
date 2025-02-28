07e7b21a6fac3687d1bba9fe20c1575135e36c41
buenorafa <rafaelbuenooo@outlook.com>
2025-02-28 03:38:56 -0300

    Adiciona a tela de adicionar sonho no MainActivity

--- app/src/main/java/br/edu/ifpb/sleepwell/MainActivity.kt
+++ app/src/main/java/br/edu/ifpb/sleepwell/MainActivity.kt
@@ -1,67 +1,42 @@
 package br.edu.ifpb.sleepwell
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import br.edu.ifpb.sleepwell.view.screens.AlarmScreen
 import br.edu.ifpb.sleepwell.model.SessionManager
 import br.edu.ifpb.sleepwell.ui.theme.SleepwellTheme
-import br.edu.ifpb.sleepwell.view.screens.BottomAppBar
+import br.edu.ifpb.sleepwell.view.screens.AddDreamScreen
 import br.edu.ifpb.sleepwell.view.screens.HomeScreen
-import br.edu.ifpb.sleepwell.view.screens.TipsScreen
 import br.edu.ifpb.sleepwell.view.screens.LoginScreen
 import br.edu.ifpb.sleepwell.view.screens.SignUpScreen
 import br.edu.ifpb.sleepwell.view.screens.SplashScreen
 
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SleepwellTheme {
-                SleepWellApp(context = this)
+                SleepWellApp()
             }
         }
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.S)
 @Composable
-fun SleepWellApp(context: Context) {
+fun SleepWellApp() {
     val navController = rememberNavController()
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = backStackEntry?.destination?.route
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            if (currentRoute !in listOf("login", "signup")) {
-                BottomAppBar(
-                    onProfileClick = { navController.navigate("profile") },
-                    onHomeClick = { navController.navigate("home") },
-                    onLogout = {
-                        SessionManager.currentUser = null
-                        navController.navigate("login")
-                    }
-                )
-            }
-        }
-    ) { innerPadding ->
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         NavHost(
             navController = navController,
             startDestination = "splash",
@@ -95,19 +70,22 @@ fun SleepWellApp(context: Context) {
             composable("home") {
                 HomeScreen(
                     userName = SessionManager.currentUser?.nome ?: "Usuário",
-                    onAlarmClick = { navController.navigate("alarm") },
-//                    onNavigateToDiary = { navController.navigate("diary") },
-                    onTipsClick = { navController.navigate("tips") },
+                    onLogout = {
+                        // Opcional: limpar a sessão no logout
+                        SessionManager.currentUser = null
+                        navController.navigate("login")
+                    }
                 )
             }
-            // Tela de alarme
-            composable("alarm") {
-                AlarmScreen(context = context)
+            // Tela Adicionar Sonho
+            composable("add-dream") {
+                AddDreamScreen(
+                    onSaveSuccess = {
+                        navController.navigate("login")
+                    }
+                )
             }
-            // Tela de dicas
-            composable("tips") {
-                TipsScreen()
-            }
+
         }
     }
 }
