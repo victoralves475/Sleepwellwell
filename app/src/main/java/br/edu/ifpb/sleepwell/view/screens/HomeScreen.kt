0cf24f3b6498e1ed5df524dd9b5a91d57f77e5c1
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2025-02-27 22:56:49 -0300

    Icon updated

--- app/src/main/java/br/edu/ifpb/sleepwell/view/screens/HomeScreen.kt
+++ app/src/main/java/br/edu/ifpb/sleepwell/view/screens/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -63,14 +64,14 @@ fun HomeScreen(
         FeatureItem(
             title = "Dicas",
             description = "Durma com os anjos",
-            icon = Icons.Default.DateRange,
-            iconBgColor = Color(0xFF76FF03) // Verde claro
+            icon = Icons.Default.Star,
+            iconBgColor = Color(0xFFFFEB3B) // Verde claro
         ),
         FeatureItem(
             title = "Monitoramento de Ciclos",
             description = "Durateston",
-            icon = Icons.Default.Star,
-            iconBgColor = Color(0xFFFFEB3B) // Amarelo claro
+            icon = Icons.Default.DateRange,
+            iconBgColor = Color(0xFF76FF03) // Amarelo claro
         ),
         FeatureItem(
             title = "Despertadores",
